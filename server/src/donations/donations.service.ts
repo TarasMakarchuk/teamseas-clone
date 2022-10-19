f9733c42f769e75992d2afefad02788d5ed1d50a
Taras Makarchuk <2278452@gmail.com>
2022-10-19 16:13:22 +0300

    add prisma service

--- server/src/donations/donations.service.ts
+++ server/src/donations/donations.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDonationInput } from './dto/create-donation.input';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class DonationsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createDonationInput: CreateDonationInput) {
     return 'This action adds a new donation';
   }
 
   findAll() {
-    return [{
-      count: 1,
-    }];
+    return this.prisma.donation.findMany();
   }
 
   findOne(id: number) {
