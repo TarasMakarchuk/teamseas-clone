8ebc94345244521e6a3dc9d41aa94f9a2d60fb95
Taras Makarchuk <2278452@gmail.com>
2022-10-20 14:30:29 +0300

    add websocket subscriptions

--- server/src/donations/donations.resolver.ts
+++ server/src/donations/donations.resolver.ts
@@ -1,15 +1,27 @@
-import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Subscription } from '@nestjs/graphql';
 import { DonationsService } from './donations.service';
 import { OrderByParams } from "../graphql";
 import { DonationCreateInput } from 'src/@generated/prisma-nestjs-graphql/donation/donation-create.input';
+import { PubSub } from 'graphql-subscriptions';
+
+const pubSub = new PubSub();
 
 @Resolver('Donation')
 export class DonationsResolver {
   constructor(private readonly donationsService: DonationsService) {}
 
   @Mutation('createDonation')
-  create(@Args('createDonationInput') createDonationInput: DonationCreateInput) {
-    return this.donationsService.create(createDonationInput);
+  async create(@Args('createDonationInput') createDonationInput: DonationCreateInput) {
+    const created = await this.donationsService.create(createDonationInput);
+    const total = await this.donationsService.getTotal();
+    await pubSub.publish('totalUpdated', { totalUpdated: { total } });
+
+    return created;
+  }
+
+  @Subscription()
+  totalUpdated() {
+    return pubSub.asyncIterator('totalUpdated');
   }
 
   @Query('donations')
