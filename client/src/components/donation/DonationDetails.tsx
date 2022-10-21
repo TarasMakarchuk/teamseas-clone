e61548c0ebb2b867f539d4f48ed7885a4e9c581d
Taras Makarchuk <2278452@gmail.com>
2022-10-21 17:33:48 +0300

    add formik custom fields

--- client/src/components/donation/DonationDetails.tsx
+++ client/src/components/donation/DonationDetails.tsx
@@ -1,5 +1,8 @@
-import {Button, VStack } from '@chakra-ui/react';
+import { Button, Heading, VStack } from '@chakra-ui/react';
 import React from 'react';
+import { Form, Formik} from "formik";
+import { InputField } from '../form/InputField';
+import { TextareaField } from '../form/TextAreaField';
 
 interface Props {
     next: (values: any) => void;
@@ -7,32 +10,77 @@ interface Props {
 }
 
 export const DonationDetails = ({ next, previous }: Props) => {
+    const submit = (values: any) => {
+        next(values);
+    };
+
     return (
-        <div>
-            <VStack>
-                <Button
-                    width="full"
-                    colorScheme="orange"
-                    size="lg"
-                    borderRadius="full"
-                    type="submit"
-                >
-                    Submit
-                </Button>
-                <Button
-                    width="full"
-                    size="lg"
-                    borderRadius="full"
-                    variant="ghost"
-                    fontSize="sm"
-                    color="gray.700"
-                    onClick={previous}
-                >
-                    Previous
-                </Button>
-            </VStack>
-        </div>
+        <Formik initialValues={{
+            displayName: '',
+            email: '',
+            mobile: '',
+            team: '',
+            message: '',
+        }}
+            onSubmit={submit}
+        >{(formikProps) => (
+            <Form>
+                <VStack spacing={4} align="stretch">
+                    <Heading as="h3" size="md">
+                        Details
+                    </Heading>
+                    <InputField
+                        label="Display Name"
+                        name="displayName"
+                        placeholder="Display name"
+                    />
+                    <InputField
+                        label="Email Address"
+                        name="email"
+                        placeholder="Email"
+                    />
+                    <InputField
+                        label="Mobile Phone"
+                        name="mobile"
+                        placeholder="Mobile Phone"
+                    />
+                    <InputField
+                        label="Team"
+                        name="team"
+                        placeholder="Team"
+                    />
+                    <TextareaField
+                        label="Message"
+                        name="message"
+                        placeholder="My #TeamSeas is..."
+                    />
+                    <hr/>
+                    <VStack>
+                        <Button
+                            width="full"
+                            colorScheme="orange"
+                            size="lg"
+                            borderRadius="full"
+                            type="submit"
+                        >
+                            Submit
+                        </Button>
+                        <Button
+                            width="full"
+                            size="lg"
+                            borderRadius="full"
+                            variant="ghost"
+                            fontSize="sm"
+                            color="gray.700"
+                            onClick={previous}
+                        >
+                            Previous
+                        </Button>
+                    </VStack>
+                </VStack>
+            </Form>
+        )}
+        </Formik>
+
     );
 };
-
-export default DonationDetails;
