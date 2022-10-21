4312f68a3dfc1bc6c93ecdf5ffe660c078a70f7d
Taras Makarchuk <2278452@gmail.com>
2022-10-21 16:36:36 +0300

    add maintaining state across steps/pages

--- client/src/components/donation/DonationWizard.tsx
+++ client/src/components/donation/DonationWizard.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, VStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { CountSelection } from "./CountSelection";
 
@@ -6,37 +6,27 @@ interface Props {}
 
 export const DonationWizard = (props: Props) => {
     const [step, setStep] = useState(0);
-    const next = (values: any = {}) => setStep(step + 1);
+    const [donationDetails, setDonationDetails] = useState({
+        count: 20,
+    });
+
+    const next = (values: any = {}) => {
+        const mergedDetails = { ...donationDetails, ...values };
+        setStep(step + 1);
+        setDonationDetails(mergedDetails);
+    };
+
     const previous = () => setStep(step - 1);
+
     const pages = [
-        <CountSelection next={next}/>,
-        <div>Page 2</div>
+        <CountSelection next={next} initialCount={donationDetails.count} />,
+        <div>
+            Page 2
+            <button onClick={previous}>prev</button>
+        </div>
     ];
 
     return <Box boxShadow="xl" p={8} bg="white" borderRadius="xl" minW="sm" >
         { pages[step] }
-
-        <VStack spacing={2}>
-            <Button
-                width="full"
-                colorScheme="orange"
-                size="lg"
-                borderRadius="full"
-                onClick={next}
-            >
-                Next
-            </Button>
-            <Button
-                width="full"
-                size="lg"
-                borderRadius="full"
-                variant="ghost"
-                fontSize="sm"
-                color="gray.700"
-                onClick={previous}
-            >
-                Previous
-            </Button>
-        </VStack>
     </Box>
 };
